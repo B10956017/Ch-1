eb7a23076354b6bb6369a724aaca6d6289a83ff9
user <user@DESKTOP-6JBEF5K>
2022-03-07 11:56:58 +0800

    練習1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,8 +6,18 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            string x = "test";
+            double meal_cost = 100;
+            int tip_precent = 15;
+            int tax_precent = 8;
+            double realtax_precent = (int)tax_precent*0.1;
+            int count = 0;
+            int total = 0;
+            count = (int)((meal_cost + tip_precent) * realtax_precent);
+            total = (int)(count + tip_precent + meal_cost);
+            Console.WriteLine("用餐費用為:" + meal_cost);
+            Console.WriteLine("服務費為:" + tip_precent);
+            Console.WriteLine("稅率為:" + tax_precent);
+            Console.WriteLine("總用餐費用為:" + total);
         }
     }
 }
